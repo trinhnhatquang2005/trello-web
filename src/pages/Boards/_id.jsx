509edcc4e14cf44e10600d555f0a95f54623ccac
Trinh Nhat Quang <trinhnhatquang99@gmail.com>
2026-06-30 15:40:01 +0700

    61.axios

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -4,12 +4,24 @@ import BoardContent from "./BoardContent/BoardContent";
 import AppBar from "~/components/AppBar/AppBar";
 import BoardBar from "./BoardBar/BoardBar";
 import { mockData } from '~/apis/mock-data'
+import { useEffect, useState } from "react"
+import { fetchBoardDetailsAPI } from "~/apis"
+
 export default function Board() {
+    const [board, setBoard] = useState(null)
+
+    useEffect(() => {
+        const boardId = '6a4376f9c8213959be357945'
+        fetchBoardDetailsAPI(boardId).then(board => {
+            setBoard(board)
+        })
+    }, [])
+
     return (
         <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
             <AppBar />
-            <BoardBar board={mockData?.board} />
-            <BoardContent board={mockData?.board} />
+            <BoardBar board={board} />
+            <BoardContent board={board} />
         </Container>
     )
 }
