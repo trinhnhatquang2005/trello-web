025ec30b38e159a1b1571b808be316fa0309f65b
Trinh Nhat Quang <trinhnhatquang99@gmail.com>
2026-07-18 16:23:03 +0700

    fix prod vercel and constant 2

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -12,7 +12,7 @@ if (import.meta.env.BUILD_MODE === 'dev') {
 // Môi trường Production sẽ cần api endpoint chuẩn của các bạn
 if (import.meta.env.BUILD_MODE === 'production') {
     // Lưu ý: Đây là domain ví dụ sau khi Deploy Production (xem video 75 và video 76 để hiểu rõ kiến thức phần này, còn hiện tại mình đã xóa domain này rồi, đừng cố truy cập làm gì =))
-    apiRoot = ''
+    apiRoot = 'https://trello-api-z3fo.onrender.com'
 }
 // console.log('🚀 ~ file: constants.js:7 ~ apiRoot:', apiRoot)
 export const API_ROOT = apiRoot
